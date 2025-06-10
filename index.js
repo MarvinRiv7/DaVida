d769ea347217ac94223515bf4699451db8880cf5
Marvin Rivas <mr2444749@gmail.com>
2025-06-10 16:46:42 -0600

    F

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express')
+const express = require('express');
+const { dbConnection } = require('./database/config');
 require('dotenv').config()
+const cors = require('cors')
 
 
 
 //Crear el servidor 
 const app = express();
 
+//DB
+dbConnection()
+
+app.use(cors())
+
 //Directorio publico
 app.use(express.static('public'))
+
+app.use(express.json())
 
 //Rutas
 app.use('/api/auth', require('./routes/auth'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,39 @@
-const {Router} = require('express')
-const router = Router()
+const { Router } = require("express");
+const {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+} = require("../controllers/auth");
+const router = Router();
+const { check } = require("express-validator");
+const { validarCapos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
+router.post(
+  "/new",
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCapos
+  ],
+  crearUsuario
+);
 
-router.get('/', (req, res) => {
-    console.log('Se require el /')
-    res.json({
-        ok: true,
-    })
-})
+router.post(
+  "/",
+  [
+    check("email", "El nombre es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCapos
+  ],
+  loginUsuario
+);
 
-module.exports = router
+router.get("/renew", validarJWT,revalidarToken);
+
+module.exports = router;
